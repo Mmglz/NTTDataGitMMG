2acfa44996433906e14d845fffc86bc21d37d39c
manoli <gonzalez.manoli94@gmail.com>
2022-04-05 17:54:41 +0200

    Primera version

---
+++ src/com/nttdata/git/App.java
@@ -0,0 +1,24 @@
+package com.nttdata.git;
+
+/**
+ * Clase principal
+ * 
+ * @author manoli
+ *
+ */
+
+public class App {
+	/**
+	 * Método principal
+	 * 
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+
+		// Impresión "Hola mundo"
+		System.out.println("Hola Mundo");
+
+	}
+
+}
